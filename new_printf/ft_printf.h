7cbc26ed1128bb990f90f5ea10bf8e18120c8776
Isaac Pizarro contreras <ipizarro@c1r9s4.42madrid.com>
2020-02-18 19:03:43 +0100

    18/02

--- new_printf/ft_printf.h
+++ new_printf/ft_printf.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ipizarro <ipizarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/27 19:18:30 by ipizarro          #+#    #+#             */
-/*   Updated: 2020/02/12 18:21:43 by ipizarro         ###   ########.fr       */
+/*   Created: 2020/02/13 16:39:05 by ipizarro          #+#    #+#             */
+/*   Updated: 2020/02/18 17:57:50 by ipizarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,16 @@ typedef struct	s_struct
 {
 	const char	*format;
 	char		*set;
-	char		*aux;
+	char		*str;
 	char		conversion;
 	va_list		args;
-	long int	nbr;
+	long int	neg;
 	long int	precision;
 	long int	width;
 	long int	asterisk_width;
 	long int	asterisk_precision;
 	char		zero;
+	int			sign;
 	int			len;
 	int			i;
 }				t_struct;
@@ -37,21 +38,28 @@ typedef struct	s_struct
 int			ft_printf(const char *format, ...);
 int			parser(t_struct *list);
 t_struct	*real_parser(t_struct *list);
-void		update_set(t_struct *list);
-void		ft_start(t_struct *list);
+t_struct	*update_set(t_struct *list);
+t_struct	*ft_start(t_struct *list);
 t_struct	*ft_precision(t_struct *list);
+t_struct	*ft_aux_precision(t_struct *list);
 t_struct	*ft_asterisk(t_struct *list);
 t_struct	*ft_width(t_struct *list);
-char		*get_number(t_struct *list, int i, char *nbr);
-char		*ft_put_precision_chars(char *str, t_struct *list);
-char		*ft_put_precision_integers(char *str, t_struct *list);
-char		*ft_put_witdh(char *str, t_struct *list);
-char		*ft_hyphen(char *str, char *new_str, t_struct *list);
-char		*ft_zeros(char *str, char *new_str, t_struct *list);
-char		*ft_sign(char *str, char *new_str, t_struct *list);
+t_struct	*ft_aux_widht(int i, int j, t_struct *list);
+char		*ft_put_precision_chars(char *new_str, t_struct *list);
+t_struct	*ft_put_precision_integers(t_struct *list);
+t_struct	*ft_aux_put_precision_integers(long int i, t_struct *list);
+t_struct	*ft_put_witdh(t_struct *list);
+t_struct	*ft_hyphen(t_struct *list);
+t_struct	*ft_spaces(t_struct *list);
+t_struct	*ft_zeros(t_struct *list);
+t_struct	*ft_sign(t_struct *list);
+t_struct	*ft_base_hexadecimal(unsigned long int k, t_struct *list);
+char		*ft_aux_hexadecimal(unsigned long int k, char *str, t_struct *list);
 t_struct	*ft_conversion(t_struct *list);
 t_struct	*ft_c_conversion(t_struct *list);
 t_struct	*ft_s_conversion(t_struct *list);
-t_struct	*ft_d_conversion(t_struct *list);
+t_struct	*ft_int_conversion(t_struct *list);
+t_struct	*ft_p_conversion(t_struct *list);
+t_struct	*list_initialize(t_struct *list);
 
 #endif
--- new_printf/test.c
+++ new_printf/test.c
@@ -3,17 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isaacpizarro95 <isaacpizarro95@student.    +#+  +:+       +#+        */
+/*   By: ipizarro <ipizarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/11 00:25:29 by isaacpizarr       #+#    #+#             */
-/*   Updated: 2020/02/16 04:57:30 by isaacpizarr      ###   ########.fr       */
+/*   Created: 2020/02/16 04:50:20 by isaacpizarr       #+#    #+#             */
+/*   Updated: 2020/02/18 16:48:05 by ipizarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <libc.h>
 
-int     main(void)
+int			main(void)
 {
-    printf("son %-8 cosas\n", 12345);
-    return (0);
+	unsigned long int i = 30;
+	void	*ptr = &i;
+
+	printf("son %8.7d cosas", -12345);
+	//getchar();
+	return (0);
 }
+
