4d3d1c0fb93279eb42b197d89ad5fdf65eb01931
Isaac Pizarro Contreras <isaacpizarro95@MacBook-Pro-de-Isaac.local>
2020-02-25 02:51:31 +0100

    25/02

---
+++ printf/ft_unsigned_itoa.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_unsigned_itoa.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isaacpizarro95 <isaacpizarro95@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/25 02:22:28 by isaacpizarr       #+#    #+#             */
+/*   Updated: 2020/02/25 02:24:57 by isaacpizarr      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char			*ft_unsigned_itoa(unsigned long int n)
+{
+	char	*str;
+	int		len;
+
+	len = ft_nbrlen(n);
+	str = ft_calloc(len + 1, sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	if (n == 0)
+		str[0] = '0';
+	while (n != 0 && len >= 0)
+	{
+		str[len-- - 1] = n % 10 + '0';
+		n = n / 10;
+	}
+	return (str);
+}
